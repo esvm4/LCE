9b4259bc4a1d23bf06a19352b8177aaff0c2dc70
Thanh Ngoc <tngocnp@pm.me>
2022-12-01 22:05:28 +0700

    change path product.json

--- js/app.js
+++ js/app.js
@@ -1,30 +1,31 @@
-var myApp = angular.module('myApp', ['ngRoute','ngSanitize']);
+var myApp = angular.module("myApp", ["ngRoute", "ngSanitize"]);
 
-myApp.config(function($routeProvider){
-    $routeProvider
-    .when('/',{
-        templateUrl: 'home.html'
+myApp.config(function ($routeProvider) {
+  $routeProvider
+    .when("/", {
+      templateUrl: "home.html",
     })
     // :slug để làm url unique và đẹp hơn
-    .when('/product/:slug',{
-        templateUrl : 'product.html',
-        controller: 'myController'
+    .when("/product/:slug", {
+      templateUrl: "product.html",
+      controller: "myController",
     })
     //khởi tạo mặc định chạy trang home
     .otherwise({
-        redirectTo: '/'
+      redirectTo: "/",
     });
 });
-myApp.controller('myController',function myController($rootScope,$scope, $location,$http){
+myApp.controller(
+  "myController",
+  function myController($rootScope, $scope, $location, $http) {
     // Gọi file json qua $http
-    $http.get('./product.json').then(function(response){
-        $scope.products=response.data.products;
+    $http.get("../json/product.json").then(function (response) {
+      $scope.products = response.data.products;
     });
     // Hàm khi click vào sản phẩm
-    $scope.productClick=function(product){
-        $location.path('/product/'+product.slug);
-        $rootScope.product=product;
+    $scope.productClick = function (product) {
+      $location.path("/product/" + product.slug);
+      $rootScope.product = product;
     };
-})
-
-  
+  }
+);
