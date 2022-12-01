b3be801b0c9ca27d432c545fc3308a8732bc8dd3
chicophi <tranhphi9c1@gmail.com>
2022-12-01 21:53:21 +0700

    update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,30 @@
 var myApp = angular.module('myApp', ['ngRoute','ngSanitize']);
 
+myApp.config(function($routeProvider){
+    $routeProvider
+    .when('/',{
+        templateUrl: 'home.html'
+    })
+    // :slug để làm url unique và đẹp hơn
+    .when('/product/:slug',{
+        templateUrl : 'product.html',
+        controller: 'myController'
+    })
+    //khởi tạo mặc định chạy trang home
+    .otherwise({
+        redirectTo: '/'
+    });
+});
+myApp.controller('myController',function myController($rootScope,$scope, $location,$http){
+    // Gọi file json qua $http
+    $http.get('./product.json').then(function(response){
+        $scope.products=response.data.products;
+    });
+    // Hàm khi click vào sản phẩm
+    $scope.productClick=function(product){
+        $location.path('/product/'+product.slug);
+        $rootScope.product=product;
+    };
+})
 
   
