aa8631765ccce27f3c8b8572cb3b14cd08f6cd62
chicophi <tranhphi9c1@gmail.com>
2022-12-04 21:26:00 +0700

    change header calling

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 var myApp = angular.module("myApp", ["ngRoute", "ngSanitize"]);
 
 myApp.config(function($routeProvider){
-    $routeProvider
+  
+  $routeProvider
     .when('/',{
         templateUrl: '../views/index.product.html'
     })
@@ -15,6 +16,10 @@ myApp.config(function($routeProvider){
     });
 });
 myApp.controller("myController",function myController($rootScope, $scope, $location, $http) {
+    // test
+    
+
+    $scope.login={};
     // Gọi file json qua $http
     $http.get('../json/product.json').then(function(response){
         $scope.products=response.data.products;
@@ -23,8 +28,50 @@ myApp.controller("myController",function myController($rootScope, $scope, $locat
     $scope.productClick = function (product) {
       $location.path("/product/" + product.slug);
       $rootScope.product = product;
+      $rootScope.cartItem={"quantity":1};
+      console.log(login);
     };
-    $scope.featureLimit=12;
+    // Hàm tăng số lượng sản phẩm
+    $rootScope.plusQuantity =function(){
+      $rootScope.cartItem.quantity++;
+      
+    }
+    // Hàm giảm số lượng sản phẩm
+    $rootScope.minusQuantity =function(){
+      if($rootScope.cartItem.quantity>1){
+        $rootScope.cartItem.quantity--;
+      }
+    }
+    // Hàm khi thêm sản phẩm vào
+    $rootScope.addToCart= function(product){
+      $rootScope.cartItem.product = product;
+      $rootScope.cartItem.total = product.price * $rootScope.cartItem.quantity;
+      if ($rootScope.cart.length>0){
+        var count=0;
+        for (var i =0; i<$rootScope.cart.length;i++){
+          if($rootScope.cart[i].product==product){
+            $rootScope.cart[i].quantity+=$rootScope.cartItem.quantity;
+            $rootScope.cart[i].total=product.price * $rootScope.cart[i].quantity;
+            count++;  
+          }  
+        };
+        if(count==0){
+          $rootScope.cartItemCopy = Object.assign({}, $rootScope.cartItem);
+          $rootScope.cart.push($rootScope.cartItemCopy);
+        }  
+      }
+      else{
+        //copy ra object giả để push
+        $rootScope.cartItemCopy = Object.assign({}, $rootScope.cartItem);
+        $rootScope.cart.push($rootScope.cartItemCopy);
+      }
+    };
+    $rootScope.cartItem={"quantity":1};
+    $rootScope.cartItemCopy={};
+    $rootScope.cart=[];
+
+    
+    
 })
 
-  
+
